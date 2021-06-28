5d78b3cfb607262bc598e0265ae8f0636c64ad12
avichai321 <77723644+avichai321@users.noreply.github.com>
2021-06-28 22:13:26 +0300

    tirgul lesson 5

---
+++ שיעור 5/lesson 5/lesson 5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_5
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintDate();
+            int age = GetMyAge();
+            Console.WriteLine(age);
+            
+            string name = GetMyName();
+            Console.WriteLine(name);
+
+
+            string f_name = Console.ReadLine();
+            string l_name = Console.ReadLine();
+            string fullname = GetFullName(f_name, l_name);
+            Console.WriteLine(fullname);
+        }
+        
+        public static void PrintDate()
+        {
+            Console.WriteLine( DateTime.Now);
+        }
+
+        public static string GetMyName()
+        {
+            return "avicii";
+        }
+        public static int GetMyAge()
+        {
+            return 24;
+        }
+
+        public static string GetFullName(string first , string last)
+        {
+            return first + " " + last;
+        }
+
+
+
+    }
+
+
+}
---
+++ שיעור 5/lesson5_tirgul/lesson5_tirgul/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5_tirgul
+{
+    class Program
+    {
+        #region 1
+        //public static void Main(string[] args)
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        Console.WriteLine("Enter word: ");
+        //        string word1 = printword(Console.ReadLine());
+        //        Console.WriteLine(word1);
+        //    }
+
+        //}
+        //public static string printword(string word)
+        //{
+        //    return word;
+        //}
+        #endregion
+
+
+        #region 2
+        //public static void Main(string[] args)
+        //{
+        //    int[] myarr = new int[3] { 0, 1, 2 };
+        //    Getarr(myarr);
+        //}
+        //public static void Getarr(int[] myarr)
+        //{
+        //    for (int i = 0; i < myarr.Length; i++)
+        //    {
+        //        Print_arr(myarr[i]);
+        //        Print_arr(myarr[i] * 2);
+
+        //    }
+        //}
+        //public static void Print_arr(int number)
+        //{
+        //    Console.WriteLine(number);
+        //}
+        #endregion
+
+        #region 3
+        public static void Main(string[] args)
+        {
+
+
+        }
+        #endregion
+
+
+
+    }
+}
