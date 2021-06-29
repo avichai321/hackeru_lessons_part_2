06a21cf7521f632baf006008d95cdb5903fcee52
avichai321 <77723644+avichai321@users.noreply.github.com>
2021-06-29 21:25:33 +0300

    home work lesson methods

---
+++ שיעור 5/lesson 5 homework/lesson 5 homework/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_5_homework
+{
+    class Program
+    {
+        #region 8
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter number between 1-10");
+        //int num = int.Parse(Console.ReadLine());
+        //    Printnumber(num);
+
+        //}
+        //public static void Printnumber(int num)
+        //{
+        //    switch(num)
+        //    {
+        //        case 1:
+        //            Console.WriteLine("one");
+        //            break;
+        //        case 2:
+        //            Console.WriteLine("two");
+        //            break;
+        //        case 3:
+        //            Console.WriteLine("three");
+        //            break;
+        //        case 4:
+        //            Console.WriteLine("four");
+        //            break;
+        //        case 5:
+        //            Console.WriteLine("five");
+        //            break;
+        //        case 6:
+        //            Console.WriteLine("six");
+        //            break;
+        //        case 7:
+        //            Console.WriteLine("seven");
+        //            break;
+        //        case 8:
+        //            Console.WriteLine("eight");
+        //            break;
+        //        case 9:
+        //            Console.WriteLine("nine");
+        //            break;
+        //        case 10:
+        //            Console.WriteLine("ten");
+        //            break;
+        //        default:
+        //            Console.WriteLine("press only 1-10");
+        //            break;
+        //    }
+        //}
+        #endregion
+
+        #region 9
+        //static void Main(string[] args)
+        //{
+        //    PrintNumberto10();
+        //    Console.WriteLine("Enter number");
+        //    int num = int.Parse(Console.ReadLine());
+        //    bool ten = PrintNumber(num);
+        //    Console.WriteLine(ten);
+        //}
+        //public static bool PrintNumber(int num)
+        //{
+        //    for (int i = 0; i <= num; i++)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //    if (num > 10)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //        return false;
+
+        //}
+        //public static void PrintNumberto10()
+        //{
+        //    for (int i = 0; i <= 10; i++)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+        #endregion
+    }
+}
