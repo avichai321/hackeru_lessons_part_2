193f25b2df9de0b6782c137e69f03db54553207b
avichai321 <77723644+avichai321@users.noreply.github.com>
2021-07-08 21:35:53 +0300

    updade lesson7 08.07.21

---
+++ שיעור 7/lesson7_class/lesson7_class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson7_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car mycar = new Car();
+            mycar.year = 2020;
+            mycar.brand = "aoodi";
+
+            Car mycar2 = new Car();
+            mycar2.year = 2021;
+            mycar2.brand = "Kia";
+            Console.WriteLine(mycar2.brand);
+        }
+    }
+    class Car
+    {
+        public int year;
+        public string brand;
+    }
+}
