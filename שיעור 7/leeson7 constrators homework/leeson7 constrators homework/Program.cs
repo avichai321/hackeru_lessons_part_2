d083da92cb9c017666b198c4208e8e8e6d222bbd
avichai321 <77723644+avichai321@users.noreply.github.com>
2021-07-11 13:13:33 +0300

    updade ctor homework 11.07.2021

---
+++ שיעור 7/leeson7 constrators homework/leeson7 constrators homework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leeson7_constrators_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 7
+            //Car c1 = new Car("ford");
+            //c1.GetBrand();
+            #endregion
+        }
+    }
+    class Car
+    {
+        public string _brand;
+        
+        public Car(string brand)
+        {
+            _brand = brand;
+        }
+        public void GetBrand()
+        {
+            Console.WriteLine(_brand);
+        }
+    }
+}
