5760d4434980a7b13af15aef7c2ffd5a7f3411a7
avichai321 <77723644+avichai321@users.noreply.github.com>
2021-07-12 21:25:01 +0300

    lesson 8 update 12/07/2021

---
+++ שיעור 8/lesson8-class/lesson8-class/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson8_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        #region 1
+            //PrintInTwoRows("one", "piece");
+            //PrintInTwoRows(1, 980);
+            //PrintInTwoRows(true, false);
+            #endregion
+
+            #region 2
+            //Calculator c1 = new Calculator();
+            //Console.WriteLine(c1.Calc(2, 5, '+'));
+            //Console.WriteLine(c1.Calc("60", "40", '-'));
+            //Console.WriteLine(c1.Calc(50,25,'/'));
+
+
+            #endregion
+
+        }
+        #region f 1
+        public static void PrintInTwoRows(string value1, string value2)
+        {
+            Console.WriteLine($"{value1}\n{value2}");
+
+        }
+
+        public static void PrintInTwoRows(int value1, int value2)
+
+        {
+            Console.WriteLine($"{value1}\n{value2}");
+        }
+
+        public static void PrintInTwoRows(bool value1, bool value2)
+        {
+            Console.WriteLine($"{value1}\n{value2}");
+        }
+        #endregion
+
+        #region f 2
+        class Calculator
+        {
+           
+            public int Calc(int num1 , int num2 , char op)
+            {
+                    switch (op)
+                    {
+                        case '+':
+                            return num1 + num2;
+                            
+
+                        case '/':
+
+                            return num1 / num2;
+                            
+
+                        case '-':
+                            return  num1 - num2;
+                            
+
+                        case '*':
+                            return num1 * num2;
+                           
+
+                    }
+                return -00;
+                
+            }
+        public  double Calc(double num1, double num2, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return num1 + num2;
+
+
+                case '/':
+
+                    return num1 / num2;
+
+
+                case '-':
+                    return num1 - num2;
+
+
+                case '*':
+                    return num1 * num2;
+
+
+            }
+            return -00;
+
+        }
+
+        public string Calc(string num1, string num2, char op)
+        {
+                int num3= int.Parse(num1);
+                int num4 =int.Parse(num2);
+            switch (op)
+            {
+                case '+':
+                    return Convert.ToString(num3 + num4);
+
+
+                case '/':
+
+                    return Convert.ToString(num3 / num4);
+
+
+                case '-':
+                    return Convert.ToString(num3 - num4);
+
+
+                case '*':
+                    return Convert.ToString(num3 * num4);
+
+
+            }
+            return "-00";
+
+        }
+        #endregion
+        }
+    }
+}
