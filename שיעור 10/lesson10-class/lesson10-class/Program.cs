9bbdcff21467d94affa9351e0d7fa476736ee041
avichai321 <77723644+avichai321@users.noreply.github.com>
2021-07-19 21:44:06 +0300

    update class 19/07/2021

--- שיעור 10/lesson10-class/lesson10-class/Program.cs
+++ שיעור 10/lesson10-class/lesson10-class/Program.cs
@@ -11,12 +11,32 @@ namespace lesson10_class
         static void Main(string[] args)
         {
             #region ref and out tirgul
-            int myint;
-            MethodWithOut(out myint);
-            MethodWithRef(ref myint);
+            //int myint;
+            //MethodWithOut(out myint);
+            //MethodWithRef(ref myint);
+
+            #endregion
+
+            #region in and params tirgul 
+
+            #region 1
+            //BuildSentence("I", "am", "Batman");
+            #endregion
+
+            #region 2
+            //Person avicii = new Person("Avicii", 24);
+            //Person daniel = new Person("Daniel", 27);
+            //Person or = new Person("Or", 27);
+            //Person avichai = new Person("Avichai", 24);
+            //Person luffy = new Person("Luffy", 21);
+
+            //avicii.PrintNameAndIncreaseAge(avicii, daniel, or, avichai, luffy);
+
+            #endregion
 
             #endregion
         }
+
         #region ref and out tirgul
         public static void MethodWithOut(out int num)
         {
@@ -28,5 +48,47 @@ namespace lesson10_class
 
         }
         #endregion
+
+        #region in and params tirgul
+        #region 1
+        public static void BuildSentence(params string[] sent)
+        {
+            for (int i = 0; i < sent.Length; i++)
+            {
+                Console.Write(sent[i] + " ");
+            }
+        }
+        #endregion
+
+        
+        #endregion
     }
+    #region 2 in and params tirgul
+
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name,int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void IncreaseAge(in Person p)
+        {
+            p.age++;
+        }
+
+        public void PrintNameAndIncreaseAge(params Person[] p)
+        {
+            for (int i = 0; i < p.Length; i++)
+            {
+                Console.WriteLine($"The name of the person {p[i].name}");
+                IncreaseAge(p[i]);
+            }
+        }
+    }
+    #endregion
 }
