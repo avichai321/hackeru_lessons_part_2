1ac684abe890e4b99b0a5ec3b9c18ba0a8fe9d5d
avichai321 <77723644+avichai321@users.noreply.github.com>
2021-07-19 19:12:16 +0300

    update class 19/07/21

---
+++ שיעור 10/lesson10-class/lesson10-class/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson10_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ref and out tirgul
+            int myint;
+            MethodWithOut(out myint);
+            MethodWithRef(ref myint);
+
+            #endregion
+        }
+        #region ref and out tirgul
+        public static void MethodWithOut(out int num)
+        {
+            num = 2;
+        }
+
+        public static void MethodWithRef(ref int num)
+        {
+
+        }
+        #endregion
+    }
+}
