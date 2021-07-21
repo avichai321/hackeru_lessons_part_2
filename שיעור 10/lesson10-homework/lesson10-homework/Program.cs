4370e383e47fc4683a5fa248d7efd4e50933368b
avichai321 <77723644+avichai321@users.noreply.github.com>
2021-07-21 23:44:59 +0300

    homework lesson 10 21/07/2021

--- שיעור 10/lesson10-homework/lesson10-homework/Program.cs
+++ שיעור 10/lesson10-homework/lesson10-homework/Program.cs
@@ -33,10 +33,37 @@ namespace lesson10_homework
             //Console.WriteLine(ifOkey);
             //Console.WriteLine(errorMessage);
             #endregion
-            
+
 
             #endregion
+
+            #region params and in names and optional args
+            #region 1
+            //Console.WriteLine(calculate(5,2,3)); 
+            #endregion
+
+            #region 2
+            //abc(5,3,3);
+            #endregion
+
+            #region 3
+            //abc(a: 1, c: 10);
+            #endregion
+
+            #region 4
+            //Console.WriteLine(function_D(3));
+            #endregion
+
+            #region 5
+            //Phone p1 = new Phone();
+            //Phone p2 = new Phone(screensize : 7, model: "oneplus");
+            //Phone p3 = new Phone(camerapixel: 67, battery: "lipo");
+            //Phone p4 = new Phone(screensize: 7, model: "oneplus nord", camerapixel: 32);
+
+            #endregion
+            #endregion
         }
+
         #region ref and out
 
         #region 2
@@ -87,5 +114,63 @@ namespace lesson10_homework
         }
         #endregion
         #endregion
+
+        #region params and in names and optional args
+        #region 1
+        public static int calculate(params int[] nums)
+        {
+            int mid_num=1;
+            int sum_num = 0;
+            for (int i = 0 ; i < nums.Length; i++)
+            {
+                for (int k = 1; k <=i+1; k++)
+                {
+                    mid_num = nums[i] * mid_num;
+                }
+                sum_num += mid_num;
+                mid_num = 1;
+            }
+            return sum_num;
+        }
+
+        #endregion
+
+        #region 2 3
+        public static void abc(int a, int b = 1 ,int c = -1)
+        {
+            Console.WriteLine(a * b * c); 
+        }
+        #endregion
+
+        #region 4
+        public static int function_D(in int num)
+        {
+            int multiply = num * 3;
+            return multiply;
+        }
+        #endregion
+
+       
+        #endregion
+
     }
+    #region params and in names and optional args
+    #region 5
+    class Phone
+    {
+        int screenSize;
+        string Model;
+        string Battery;
+        int cameraPixels;
+
+        public Phone(int screensize = 6 , string model = "samsung" , string battery = "lion", int camerapixel = 50)
+        {
+            screenSize = screensize;
+            Model = model;
+            Battery = battery;
+            cameraPixels = camerapixel;
+        }
+    }
+    #endregion
+    #endregion
 }
