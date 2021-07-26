60e0b89398d92f7af22405bf2036444552aa59e9
avichai321 <77723644+avichai321@users.noreply.github.com>
2021-07-26 21:24:31 +0300

    lesson12-class

---
+++ שיעור 12/lesson 12-class/lesson 12-class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_12_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region properties
+            Table t1 = new Table("blue");
+            Console.WriteLine(t1.Price);
+            Console.WriteLine(t1.Manufacturer);
+            
+            
+            #endregion
+        }
+    }
+}
---
+++ שיעור 12/lesson 12-class/lesson 12-class/Table.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_12_class
+{
+   public class Table
+    {
+        public string Color;
+        private int price;
+        public int Price { get{ return price; } private set {price=value;} }
+        public string Manufacturer { get; } = "bamaba";
+        int tprice = 50;
+        
+        public Table(string color)
+        {
+            Color = color;
+            price = tprice;
+        }
+        
+        
+
+        
+        
+    }
+}
