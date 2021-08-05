22d7bad3e59876446adf438cdc8c49e224304b08
avichai321 <77723644+avichai321@users.noreply.github.com>
2021-08-05 21:36:36 +0300

    update 05/08/21

---
+++ שיעור 15/lesson 15 - class/lesson 15 - class/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_15___class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region foreach
+            student[] grade = new student[3] { new student(90), new student(85), new student(95) };
+            foreach (student StudentGrade in grade)
+            {
+                Console.WriteLine(StudentGrade.Grade);
+            }
+            #endregion
+        }
+    }
+    class student
+    {
+        public student(double grade)
+        {
+            Grade = grade;
+        }
+
+        public double Grade { get; set; }
+    }
+    
+}
